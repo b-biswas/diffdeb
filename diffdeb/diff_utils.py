4c4ff2ffc2fe936139e47e53d60b6ec0b259efe6
b-biswas <bbiswas.dgp@gmail.com>
2024-01-01 12:16:35 +0100

    forward noise

---
+++ diffdeb/diff_utils.py
@@ -0,0 +1,27 @@
+import jax.numpy as jnp
+import jax.random as random
+from ml_collections import config_flags
+
+# Defining a constant value for T
+timesteps = 200
+
+# Defining beta for all t's in T steps
+beta = jnp.linspace(0.0001, 0.02, timesteps)
+
+# Defining alpha and its derivatives according to reparameterization trick
+alpha = 1 - beta
+alpha_bar = jnp.cumprod(alpha, 0)
+alpha_bar = jnp.concatenate((jnp.array([1.]), alpha_bar[:-1]), axis=0)
+sqrt_alpha_bar = jnp.sqrt(alpha_bar)
+one_minus_sqrt_alpha_bar = jnp.sqrt(1 - alpha_bar)
+
+# Implement noising logic according to reparameterization trick
+def forward_noising(key, x_0, t):
+  noise = random.normal(key, x_0.shape)  
+  reshaped_sqrt_alpha_bar_t = jnp.reshape(jnp.take(sqrt_alpha_bar, t), (-1, 1, 1, 1))
+  reshaped_one_minus_sqrt_alpha_bar_t = jnp.reshape(jnp.take(one_minus_sqrt_alpha_bar, t), (-1, 1, 1, 1))
+  noisy_image = reshaped_sqrt_alpha_bar_t  * x_0 + reshaped_one_minus_sqrt_alpha_bar_t  * noise
+  return noisy_image, noise
+
+
+
