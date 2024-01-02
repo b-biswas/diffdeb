86b0302a7f1554e582ef067b8bfb32ba70e90597
b-biswas <bbiswas.dgp@gmail.com>
2024-01-03 00:06:55 +0100

    jit functions

--- diffdeb/train.py
+++ diffdeb/train.py
@@ -1,4 +1,4 @@
-from diffdeb.losses import kl_divergence, mse_loss_fn
+from diffdeb.losses import kl_divergence, mse_loss_fn, vae_train_loss
 from diffdeb.models import create_vae_model, UNet
 import jax 
 import jax.numpy as jnp
@@ -6,6 +6,8 @@ import numpy as np
 from jax import random 
 import logging
 from diffdeb.diff_utils import forward_noising
+import time
+from functools import partial
 
 import ml_collections
 
@@ -15,44 +17,66 @@ import orbax
 from flax.training import orbax_utils
 import optax
 
+logging.basicConfig(level=logging.INFO)
+
+@jax.jit
 def compute_metrics_vae(recon_x, x, mean, logvar):
   mse_loss = mse_loss_fn(prediction=recon_x, truth=x).mean()
   kld_loss = kl_divergence(mean, logvar).mean()
   return {'mse': mse_loss, 'kld': kld_loss, 'loss': mse_loss + kld_loss}
 
-def train_step_vae(state, batch, z_rng, latent_dim):
+@partial(jax.jit, static_argnames=['latent_dim', 'input_shape', 'encoder_filters', 'encoder_kernels', 'decoder_filters', 'decoder_kernels', 'dense_layer_units'])
+def train_step_vae(state, batch, z_rng, latent_dim, input_shape, encoder_filters, encoder_kernels, decoder_filters, decoder_kernels, dense_layer_units):
   def loss_fn(params):
-    recon_x, mean, logvar = create_vae_model(latent_dim).apply(
-        {'params': params}, batch[0], z_rng
-    )
+    recon_x, mean, logvar = create_vae_model(
+      latent_dim, 
+      input_shape, 
+      encoder_filters, 
+      encoder_kernels, 
+      decoder_filters,
+      decoder_kernels, 
+      dense_layer_units
+    ).apply({'params': params}, batch[0], z_rng)
 
-    mse_loss = mse_loss_fn(recon_x, batch[1]).mean()
-    kld_loss = kl_divergence(mean, logvar).mean()
-    loss = mse_loss + kld_loss
+    loss=vae_train_loss(
+      prediction=recon_x, 
+      truth=batch[1],
+      mean=mean, 
+      logvar=logvar,
+    )
     return loss
 
   loss, grads = jax.value_and_grad(loss_fn, argnums=0)(state.params)
-  return state.apply_gradients(grads=grads), jnp.mean(loss)
+  return state.apply_gradients(grads=grads), loss.mean()
 
-
-def eval_f_vae(params, images, z, z_rng, latent_dim):
+@partial(jax.jit, static_argnames=['latent_dim', 'input_shape', 'encoder_filters', 'encoder_kernels', 'decoder_filters', 'decoder_kernels', 'dense_layer_units'])
+def eval_f_vae(params, images, z_rng, latent_dim, input_shape, encoder_filters, encoder_kernels, decoder_filters, decoder_kernels, dense_layer_units):
   def eval_model(vae):
     recon_images, mean, logvar = vae(images[0], z_rng)
-    comparison = jnp.concatenate([
-        images[1].reshape(-1, 45, 45, 6),
-        recon_images.reshape(-1, 45, 45, 6),
-    ])
+    # comparison = jnp.concatenate([
+    #     images[1].reshape(-1, 45, 45, 6),
+    #     recon_images.reshape(-1, 45, 45, 6),
+    # ])
 
-    generate_images = vae.generate(z)
-    generate_images = generate_images.reshape(-1, 45, 45, 6)
+    # generate_images = vae.generate(z)
+    # generate_images = generate_images.reshape(-1, 45, 45, 6)
     metrics = compute_metrics_vae(recon_images, images[1], mean, logvar)
-    return metrics, comparison, generate_images
+    #return metrics, comparison, generate_images
+    return metrics
 
-  return nn.apply(eval_model, create_vae_model(latent_dim))({'params': params})
+  return nn.apply(eval_model, create_vae_model(
+      latent_dim, 
+      input_shape, 
+      encoder_filters, 
+      encoder_kernels, 
+      decoder_filters,
+      decoder_kernels, 
+      dense_layer_units,
+    ))({'params': params})
 
 def train_and_evaluate_vae(
-    train_ds,
-    val_ds,
+    train_tfds,
+    val_tfds,
     config: ml_collections.ConfigDict,
   ):
   """Train and evaulate pipeline."""
@@ -70,66 +94,110 @@ def train_and_evaluate_vae(
 
   logging.info('Initializing model.')
   init_data = jnp.ones((config.batch_size, 45, 45, 6), jnp.float32)
-  params = create_vae_model(latent_dim=16).init(key, init_data, rng)['params']
+  params = create_vae_model(
+    config.latent_dim, 
+    config.input_shape, 
+    config.encoder_filters, 
+    config.encoder_kernels, 
+    config.decoder_filters,
+    config.decoder_kernels, 
+    config.dense_layer_units
+  ).init(key, init_data, rng)['params']
 
   state = train_state.TrainState.create(
-      apply_fn=create_vae_model(latent_dim=16).apply,
+      apply_fn=create_vae_model(
+        config.latent_dim, 
+        config.input_shape, 
+        config.encoder_filters, 
+        config.encoder_kernels, 
+        config.decoder_filters,
+        config.decoder_kernels, 
+        config.dense_layer_units
+      ).apply,
       params=params,
       tx=optax.adam(config.learning_rate),
   )
 
-  rng, z_key, eval_rng = random.split(rng, 3)
-  z = random.normal(z_key, (32, config.latent_dim))
+  #rng, z_key, eval_rng = random.split(rng, 3)
+  #z = random.normal(z_key, (32, config.latent_dim))
   
   min_val_loss = np.inf
+  logging.info('Training started...')
+  metrics = {"val loss": [], "val mse": [], "val kld": [], "train loss": []}
   for epoch in range(config.num_epochs):
-    train_loss = []
+    train_ds = train_tfds.as_numpy_iterator()
+    start = time.time()
+    metrics["train loss"].append(0.0)
     for _ in range(config.steps_per_epoch_train):
       batch = next(train_ds)
       rng, key = random.split(rng)
-      state, batch_train_loss = train_step_vae(state, batch, key, config.latent_dim)
-      train_loss.append(batch_train_loss)
+      state, batch_train_loss = train_step_vae(
+        state, 
+        batch, 
+        key, 
+        config.latent_dim, 
+        input_shape=config.input_shape,
+        encoder_filters=config.encoder_filters,
+        encoder_kernels=config.encoder_kernels,
+        decoder_filters=config.decoder_filters,
+        decoder_kernels=config.decoder_kernels,
+        dense_layer_units=config.dense_layer_units,
+      )
+      metrics["train loss"][epoch] = compute_av(
+        metrics["train loss"][epoch], 
+        batch_train_loss, 
+        config.steps_per_epoch_train,
+      )
+    val_ds = val_tfds.as_numpy_iterator()
+    for loss_name in ["loss", "mse", "kld"]:
+      metrics["val "+loss_name].append(0.0)
 
-    metrics = {"val loss": 0.0, "val mse": 0.0, "val kld": 0.0}
-    
     for _ in range(config.steps_per_epoch_val):
       batch = next(val_ds)
       rng, key = random.split(rng)
-      batch_metrics, comparison, sample = eval_f_vae(
+      batch_metrics = eval_f_vae(
         params=state.params, 
         images=batch, 
-        z=z, 
         z_rng=key, 
         latent_dim=config.latent_dim,
+        input_shape=config.input_shape, 
+        encoder_filters=config.encoder_filters,
+        encoder_kernels=config.encoder_kernels,
+        decoder_filters=config.decoder_filters,
+        decoder_kernels=config.decoder_kernels,
+        dense_layer_units=config.dense_layer_units,
       )
       for loss_name in ["loss", "mse", "kld"]:
-        metrics["val "+loss_name] = compute_av(
-          metrics["val "+loss_name], 
+        metrics["val "+loss_name][epoch] = compute_av(
+          metrics["val "+loss_name][epoch], 
           batch_metrics[loss_name], 
           config.steps_per_epoch_val,
         )
-    metrics["train loss"]=np.mean(train_loss)
+
     # vae_utils.save_image(
     #     comparison, f'results/reconstruction_{epoch}.png', nrow=8
     # )
     # vae_utils.save_image(sample, f'results/sample_{epoch}.png', nrow=8)
-    print('Previous minimum validation loss; {:.5f}'.format(min_val_loss))
-    print(
-        'eval epoch: {}, train loss: {:.4f}, val loss: {:.4f}, val MSE: {:.4f}, val kld: {:.4f}'.format(
+    logging.info('Previous minimum validation loss: {:.7f}'.format(min_val_loss))
+    logging.info(
+        'eval epoch: {}, train loss: {:.7f}, val loss: {:.7f}, val MSE: {:.7f}, val kld: {:.7f}'.format(
             epoch + 1,
-            metrics["train loss"],
-            metrics["val loss"],
-            metrics["val mse"], 
-            metrics["val kld"],
+            metrics["train loss"][epoch],
+            metrics["val loss"][epoch],
+            metrics["val mse"][epoch], 
+            metrics["val kld"][epoch],
         )
     )
-    if metrics["val loss"]<min_val_loss:
-      print("Saving model at "+config.model_path)
-      min_val_loss = metrics["val loss"]
+    stop = time.time()
+    logging.info('Duration {:.5f}'.format(stop-start))
+    if metrics["val loss"][epoch] < min_val_loss:
+      
+      min_val_loss = metrics["val loss"][epoch]
       ckpt = {'model': state, 'config': config}
 
       save_args = orbax_utils.save_args_from_target(ckpt)
       checkpoint_manager.save(epoch, ckpt, save_kwargs={'save_args': save_args})
+      logging.info("Model saved at "+config.model_path)
 
 
 
@@ -142,6 +210,7 @@ def eval_f_UNet(params, images, timestamps):
 
   return nn.apply(eval_model, UNet())({'params': params})
 
+@jax.jit
 def train_step_UNet(state, batch, timestamps):
   def loss_fn(params):
     recon_x = UNet().apply(
@@ -156,8 +225,8 @@ def train_step_UNet(state, batch, timestamps):
   return state.apply_gradients(grads=grads), jnp.mean(loss)
 
 def train_and_evaluate_UNet(
-    train_ds,
-    val_ds,
+    train_tfds,
+    val_tfds,
     config: ml_collections.ConfigDict,
   ):
   """Train and evaulate pipeline."""
@@ -195,8 +264,10 @@ def train_and_evaluate_UNet(
   )
   min_val_loss = np.inf
 
-  for epoch in range(config.num_epochs):
+  logging.info("start training...")
 
+  for epoch in range(config.num_epochs):
+    train_ds = train_tfds.as_numpy_iterator()
     # run over training steps
     train_loss = []
     for _ in range(config.steps_per_epoch_train):
@@ -218,7 +289,7 @@ def train_and_evaluate_UNet(
     # run over validation steps
     metrics = {"val loss": 0.0} 
     for _ in range(config.steps_per_epoch_val):
-      batch = next(val_ds)
+      val_ds = val_tfds.as_numpy_iterator()
       rng, key = random.split(rng)
       timestamps = random.randint(
         key, 
@@ -246,8 +317,8 @@ def train_and_evaluate_UNet(
     #     comparison, f'results/reconstruction_{epoch}.png', nrow=8
     # )
     # vae_utils.save_image(sample, f'results/sample_{epoch}.png', nrow=8)
-    print('Previous minimum validation loss; {:.5f}'.format(min_val_loss))
-    print(
+    logging.info('Previous minimum validation loss; {:.5f}'.format(min_val_loss))
+    logging.info(
         'eval epoch: {}, train loss: {:.4f}, val loss: {:.4f}'.format(
             epoch + 1,
             metrics["train loss"],
@@ -256,7 +327,7 @@ def train_and_evaluate_UNet(
     )
 
     if metrics["val loss"]<min_val_loss:
-      print("Saving model at "+config.model_path)
+      logging.info("Saving model at "+config.model_path)
       min_val_loss = metrics["val loss"]
       ckpt = {'model': state, 'config': config}
 
